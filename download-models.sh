a4187dcebea6e9393bd2ac5464635464d34dcdc8
Amine-AH <156811604+Amine-AH@users.noreply.github.com>
2025-02-25 19:23:23 +0100

    Update download-models.sh
    trying to solve an error related to an expected argument

--- download-models.sh
+++ download-models.sh
@@ -3,4 +3,6 @@ pip install gdown==5.2.0
 gdrive_folder_path="$1"
 
 
-python download-models.py --gdrive_folder_path "$gdrive_folder_path"
+python download-models.py "$gdrive_folder_path"
+
+#python download-models.py --gdrive_folder_path "$gdrive_folder_path"
