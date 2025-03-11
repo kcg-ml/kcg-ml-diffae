a09a0a9a6f14a032de87ea3db5b32f79e9b38cd7
rachid2198 <rachid9821@gmail.com>
2025-03-11 23:00:38 +0100

    test using a random Xt

--- training/examples/example_diffae_inference.py
+++ training/examples/example_diffae_inference.py
@@ -105,7 +105,7 @@ class DiffAEInferencePipeline:
             
             cond = self.diffae.encode(x)
             
-            xT = self.diffae.encode_stochastic(x, cond, T=2500)
+            xT = self.diffae.encode_stochastic(x, cond, T=500)
             
             pred = self.diffae.render(xT, cond, T=20)
 
