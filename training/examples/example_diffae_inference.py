ab8685ff99315197f2f7b7411b029cbca052871c
rachid2198 <rachid9821@gmail.com>
2025-03-11 23:36:44 +0100

    test using a different timestep

--- training/examples/example_diffae_inference.py
+++ training/examples/example_diffae_inference.py
@@ -105,7 +105,8 @@ class DiffAEInferencePipeline:
             
             cond = self.diffae.encode(x)
             
-            xT = self.diffae.encode_stochastic(x, cond, T=500)
+            xT = torch.randn_like(x)
+            # xT = self.diffae.encode_stochastic(x, cond, T=500)
             
             pred = self.diffae.render(xT, cond, T=20)
 
